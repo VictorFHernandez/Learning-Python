d31672ea90f8c316e1a5c1db7e19182fb5dbb527
VictorFHernandez <victorfhernandez3@gmail.com>
2021-07-29 10:47:45 -0500

    did a sum two input numbers exercise

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 Hello = "Hello World"
-price = 100.95
-first_name = "victor"
-is_online = False
 
 print(Hello)
 
@@ -11,3 +8,7 @@ print("Hello " + users_name)
 birth_year = input("What is your birth year? ")
 age = 2021 - int(birth_year)
 print("Your age is " + str(age))
+
+number1 = input("Enter a number ")
+number2 = input("Enter a second number ")
+print(int(number1) + int(number2))
